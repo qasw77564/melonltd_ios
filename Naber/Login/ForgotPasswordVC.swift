461d0745221b704f1f146171496f92095b4517d5
ChenZiLong <qasw77564@gmail.com>
2018-08-01 16:51:33 +0800

    0801忘記密碼Code OK

--- Naber/Login/ForgotPasswordVC.swift
+++ Naber/Login/ForgotPasswordVC.swift
@@ -5,7 +5,6 @@
 //  Created by LianYong-Jun on 2018/5/22.
 //  Copyright © 2018年 Melone.L.T.D. All rights reserved.
 //
-
 import UIKit
 
 class ForgotPasswordVC: UIViewController {
@@ -24,35 +23,46 @@ class ForgotPasswordVC: UIViewController {
     @IBAction func sendToServerForAskPassword(_ sender: Any) {
         
         if judgmentInput(){
-//            let reqDate : ReqData = ReqData()
-//            reqDate.phone = self.phone.text
-//            ApiManager.forgetPassword( req: reqDate, ui: self, onSuccess: {
-//                let alert = UIAlertController(title: "", message: "感謝你註冊成為商家你，\n您的信息已經提交成功，\n請待客服與您聯繫!!" , preferredStyle: .alert)
-//                alert.addAction(UIAlertAction.init(title: "返回登入畫面", style: .default, handler: { _ in
-//                    if let vc = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "LoginHomeRoot") as? LoginHomeRootUINC {
-//                        self.present(vc, animated: false, completion: nil)
-//                    }
-//                }, onFail: { (err_msg) in
-//
-//                }
-//            }
+            let reqDate : ReqData = ReqData()
+            reqDate.email = self.email.text
+            reqDate.phone = self.phone.text
+            ApiManager.forgetPassword(req: reqDate, ui: self, onSuccess: {
+                let alert = UIAlertController(title: "", message: "密碼已經傳送至於您的手機，請於簡訊功能查看！" , preferredStyle: .alert)
+                alert.addAction(UIAlertAction.init(title: "關閉", style: .default, handler: { _ in
+                    if let vc = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "LoginHomeRoot") as? LoginHomeRootUINC {
+                        self.present(vc, animated: false, completion: nil)
+                    }
+                }))
+                self.present(alert, animated: false)
+            }) { err_msg in
+                let alert = UIAlertController(title: "", message: err_msg , preferredStyle: .alert)
+                alert.addAction(UIAlertAction.init(title: "cancel", style: .cancel, handler: { _ in
+                    
+                }))
+                self.present(alert, animated: false)
+            }
         }
     }
 //    找回密碼判斷
-            func judgmentInput() -> Bool {
-                var msg: String = ""
-                if(ValidateHelper.shared.isVaildTelPhone(withCellPhone: self.phone.text!) ){
-                    msg = "請輸入正確的手機號碼"
-                }
-                if self.phone.text == "" {
-                    msg = "手機號碼不可為空"
-                }
-                if msg != "" {
-                    let alert = UIAlertController(title: "", message: msg,   preferredStyle: .alert)
-                    alert.addAction(UIAlertAction(title: "關閉", style: .cancel, handler: { _ in
-                    }))
-                    self.present(alert, animated: false)
-                }
-                return msg == ""
+        func judgmentInput() -> Bool {
+            var msg: String = ""
+            var email: String = ""
+            if(!ValidateHelper.shared.isVaildTelPhone(withCellPhone: self.phone.text!) ){
+                msg = "請輸入正確的手機號碼"
             }
+            if self.phone.text == "" {
+                msg = "手機號碼不可為空"
+            }
+            if msg != "" {
+                let alert = UIAlertController(title: "", message: msg,   preferredStyle: .alert)
+                alert.addAction(UIAlertAction(title: "關閉", style: .cancel, handler: { _ in
+                }))
+                self.present(alert, animated: false)
+            }
+            return msg == ""
+        }
+    // 鍵盤點擊背景縮放
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
+    }
 }
